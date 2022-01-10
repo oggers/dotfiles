b37f99fdc17950d672ac62a3c7f78770a1f037e2
Juan Carlos Coruña <oggers@gmail.com>
2022-01-10 22:37:00 +0100

    Add qtile role

---
+++ roles/qtile/files/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+monitors=`xrandr --listmonitors | grep "+" | awk {'print $4'} | wc -l`
+if [ $monitors -eq "2" ]; then
+    xrandr --output eDP --auto --output HDMI-A-0 --auto --left-of eDP
+fi
+
+nm-applet &
+volumeicon &
+picom &
+megasync &
+cbatticon -u 20 -c "poweroff" -l 15 -r 3 &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
