5433e904e904deea73b7aa88f59ad4158e35a0c4
Juan Carlos Coruña <oggers@gmail.com>
2025-02-26 22:36:19 +0100

    Add megasync to floating window

--- roles/qtile/files/qtile/config.py
+++ roles/qtile/files/qtile/config.py
@@ -407,6 +407,7 @@ floating_layout = layout.Floating(
         Match(wm_class='kdenlive'),       # kdenlive
         Match(wm_class="makebranch"),     # gitk
         Match(wm_class="maketag"),        # gitk
+        Match(wm_class="megasync"),       # megasync
         Match(wm_class="notification"),   # notifications
         Match(wm_class="pavucontrol"),    # pavucontrol
         Match(wm_class='pinentry-gtk-2'), # GPG key password entry
